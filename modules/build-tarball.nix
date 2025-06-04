f32859a75ce17d95b3499f4111ae391493588964
Emmanuel Alap <15620712+ealap@users.noreply.github.com>
2025-06-04 13:41:14 +0200

    fork: Apply customizations
    Signed-off-by: Emmanuel Alap <15620712+ealap@users.noreply.github.com>

--- modules/build-tarball.nix
+++ modules/build-tarball.nix
@@ -25,31 +25,21 @@ let
     else "release-" + version;
 
   defaultConfig = pkgs.writeText "default-configuration.nix" ''
-    # Edit this configuration file to define what should be installed on
-    # your system. Help is available in the configuration.nix(5) man page, on
-    # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
-    # NixOS-WSL specific options are documented on the NixOS-WSL repository:
-    # https://github.com/nix-community/NixOS-WSL
-
     { config, lib, pkgs, ... }:
 
     {
-      imports = [
-        # include NixOS-WSL modules
-        <nixos-wsl/modules>
-      ];
+      imports = [ ];
+
+      nix.settings = {
+        "flakes"
+        "nix-command"
+      };
 
       wsl.enable = true;
       wsl.defaultUser = "${config.wsl.defaultUser}";
+      wsl.defaultUser = "ealap";
 
-      # This value determines the NixOS release from which the default
-      # settings for stateful data, like file locations and database versions
-      # on your system were taken. It's perfectly fine and recommended to leave
-      # this value at the release version of the first install of this system.
-      # Before changing this value read the documentation for this option
-      # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-      system.stateVersion = "${config.system.nixos.release}"; # Did you read the comment?
+      system.stateVersion = "${config.system.nixos.release}";
     }
   '';
 in
@@ -100,6 +90,8 @@ in
 
         echo "[NixOS-WSL] Adding channel..."
         nixos-enter --root "$root" --command 'HOME=/root nix-channel --add https://github.com/nix-community/NixOS-WSL/archive/refs/heads/${nixosWslBranch}.tar.gz nixos-wsl'
+        nixos-enter --root "$root" --command 'HOME=/root nix-channel --add https://nixos.org/channels/nixos-unstable nixos'
+        nixos-enter --root "$root" --command 'HOME=/root nix-channel --add https://github.com/nix-community/NixOS-WSL/archive/refs/heads/main.tar.gz nixos-wsl'
 
         echo "[NixOS-WSL] Adding wsl-distribution.conf"
         install -Dm644 ${wsl-distribution-conf} "$root/etc/wsl-distribution.conf"
